273e2dc319dab11b067b8db430d4e109df5d6ce9
ctkrecords <ctkrecordings@gmail.com>
2019-08-27 20:51:48 -0600

    ADDED: functionality to buttons

--- js/effects.js
+++ js/effects.js
@@ -5,18 +5,21 @@ $(function () {
 	});
 });
 
+
 function cursos_desarrollo(){
+	var cde = document.getElementById("curso_desarrollo").style.display;
 	var ce = document.getElementById("curso_estrategia").style.display;
 	var cd = document.getElementById("curso_design").style.display;
 	var cem = document.getElementById("curso_emprendimiento").style.display;
 	var cdp = document.getElementById("curso_design_pay").style.display;
 	var cemp = document.getElementById("curso_emprendimiento_pay").style.display;
-	if(ce=='block'){
+	if((ce && cd && cem && cdp && cemp)=='block'){
 		document.getElementById("curso_estrategia").style.display='none';
 		document.getElementById("curso_design").style.display='none';
 		document.getElementById("curso_emprendimiento").style.display='none';
 		document.getElementById("curso_design_pay").style.display='none';
 		document.getElementById("curso_emprendimiento_pay").style.display='none';
+		document.getElementById("curso_desarrollo").style.display='block';
 	}else{
 		document.getElementById("curso_estrategia").style.display='block';
 		document.getElementById("curso_design").style.display='block';
@@ -27,21 +30,68 @@ function cursos_desarrollo(){
 }
 
 function cursos_emprendimiento(){
-	console.log("click");
 	var cde = document.getElementById("curso_desarrollo").style.display;
 	var cd = document.getElementById("curso_design").style.display;
+	var ce = document.getElementById("curso_estrategia").style.display;
 	var cem = document.getElementById("curso_emprendimiento").style.display;
 	var cdp = document.getElementById("curso_design_pay").style.display;
 	var cemp = document.getElementById("curso_emprendimiento_pay").style.display;
-	if(cd=='block'){
+	if((ce && cd  && cdp && cde)=='block'){
 		document.getElementById("curso_estrategia").style.display='none';
 		document.getElementById("curso_desarrollo").style.display='none';
 		document.getElementById("curso_design").style.display='none';
 		document.getElementById("curso_design_pay").style.display='none';
+		document.getElementById("curso_emprendimiento_pay").style.display='block';
+		document.getElementById("curso_emprendimiento").style.display='block';
 	}else{
 		document.getElementById("curso_estrategia").style.display='block';
 		document.getElementById("curso_design").style.display='block';
 		document.getElementById("curso_design_pay").style.display='block';
+		document.getElementById("curso_desarrollo").style.display='block';
+	}
+}
+
+function cursos_estrategia(){
+	var cde = document.getElementById("curso_desarrollo").style.display;
+	var ce = document.getElementById("curso_estrategia").style.display;
+	var cd = document.getElementById("curso_design").style.display;
+	var cem = document.getElementById("curso_emprendimiento").style.display;
+	var cdp = document.getElementById("curso_design_pay").style.display;
+	var cemp = document.getElementById("curso_emprendimiento_pay").style.display;
+	if((cde && cd && cem && cdp && cemp)=='block'){
+		document.getElementById("curso_estrategia").style.display='block';
+		document.getElementById("curso_design").style.display='none';
+		document.getElementById("curso_emprendimiento").style.display='none';
+		document.getElementById("curso_design_pay").style.display='none';
+		document.getElementById("curso_emprendimiento_pay").style.display='none';
+		document.getElementById("curso_desarrollo").style.display='none';
+	}else{
+		document.getElementById("curso_desarrollo").style.display='block';
+		document.getElementById("curso_design").style.display='block';
+		document.getElementById("curso_emprendimiento").style.display='block';
+		document.getElementById("curso_design_pay").style.display='block';
+		document.getElementById("curso_emprendimiento_pay").style.display='block';
+	}
+}
+
+function cursos_design(){
+	var cde = document.getElementById("curso_desarrollo").style.display;
+	var cd = document.getElementById("curso_design").style.display;
+	var ce = document.getElementById("curso_estrategia").style.display;
+	var cem = document.getElementById("curso_emprendimiento").style.display;
+	var cdp = document.getElementById("curso_design_pay").style.display;
+	var cemp = document.getElementById("curso_emprendimiento_pay").style.display;
+	if((cde && ce  && cem && cemp)=='block'){
+		document.getElementById("curso_estrategia").style.display='none';
+		document.getElementById("curso_desarrollo").style.display='none';
+		document.getElementById("curso_design").style.display='block';
+		document.getElementById("curso_design_pay").style.display='block';
+		document.getElementById("curso_emprendimiento_pay").style.display='none';
+		document.getElementById("curso_emprendimiento").style.display='none';
+	}else{
+		document.getElementById("curso_estrategia").style.display='block';
+		document.getElementById("curso_emprendimiento").style.display='block';
+		document.getElementById("curso_emprendimiento_pay").style.display='block';
 		document.getElementById("curso_desarrollo").style.display='block';
 	}
 }
